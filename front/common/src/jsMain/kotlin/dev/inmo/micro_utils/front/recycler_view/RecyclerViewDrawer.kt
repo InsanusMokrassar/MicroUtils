0b27b5cc069b231c3f65af1d9f0b3a9b1d81ab1c
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-10-28 13:51:24 +0600

    Revert "start add recycler view"
    This reverts commit 347e9c32fe08e629525a01da2e5533368b44af5d.

--- front/common/src/jsMain/kotlin/dev/inmo/micro_utils/front/recycler_view/RecyclerViewDrawer.kt
+++
@@ -1,8 +0,0 @@
-package dev.inmo.micro_utils.front.recycler_view
-
-import org.w3c.dom.HTMLElement
-
-interface RecyclerViewDrawer {
-    fun drawBefore(what: HTMLElement, before: HTMLElement)
-    fun drawAfter(what: HTMLElement, after: HTMLElement)
-}
--- front/common/src/jsMain/kotlin/dev/inmo/micro_utils/front/recycler_view/RecyclerViewElement.kt
+++
@@ -1,9 +0,0 @@
-package dev.inmo.micro_utils.front.recycler_view
-
-import org.w3c.dom.HTMLElement
-
-interface RecyclerViewElement {
-    val element: HTMLElement
-    suspend fun onOutsideOfViewport()
-    suspend fun onInsideOfViewport()
-}
