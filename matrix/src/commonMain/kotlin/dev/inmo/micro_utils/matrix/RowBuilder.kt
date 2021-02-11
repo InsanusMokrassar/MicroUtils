b120fbd2b11f99bc69f5776f72fe517ae04d3e4f
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-02-11 15:07:24 +0600

    remove add and put column method in matrix row builder

--- matrix/src/commonMain/kotlin/dev/inmo/micro_utils/matrix/RowBuilder.kt
+++ matrix/src/commonMain/kotlin/dev/inmo/micro_utils/matrix/RowBuilder.kt
@@ -5,11 +5,10 @@ class RowBuilder<T> {
     val row: Row<T>
         get() = mutRow
 
-    fun add(t: T) = mutRow.add(t)
-    operator fun T.unaryPlus() = add(this)
+    fun column(t: T) = mutRow.add(t)
+    operator fun T.unaryPlus() = column(this)
 }
 
 fun <T> row(block: RowBuilder<T>.() -> Unit): List<T> = RowBuilder<T>().also(block).row
-fun <T> RowBuilder<T>.column(element: T) = +element
 fun <T> RowBuilder<T>.columns(elements: List<T>) = elements.forEach(::column)
 fun <T> RowBuilder<T>.columns(vararg elements: T) = elements.forEach(::column)
