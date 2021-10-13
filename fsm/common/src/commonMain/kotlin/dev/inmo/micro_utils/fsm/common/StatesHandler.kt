e9c5df4c131b2469d011b2cfbfecb7c7e996d0a6
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-10-13 15:09:05 +0600

    upfill kdocs

--- fsm/common/src/commonMain/kotlin/dev/inmo/micro_utils/fsm/common/StatesHandler.kt
+++ fsm/common/src/commonMain/kotlin/dev/inmo/micro_utils/fsm/common/StatesHandler.kt
@@ -5,7 +5,8 @@ package dev.inmo.micro_utils.fsm.common
  */
 fun interface StatesHandler<I : State> {
     /**
-     *
+     * Main handling of [state]. In case when this [state] leads to another [State] and [handleState] returns not null
+     * [State] it is assumed that chain is not completed.
      */
     suspend fun StatesMachine.handleState(state: I): State?
 }
