2c2b3641674ac821253b90ea3faaf8e5e1a5f319
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-08-22 01:25:07 +0600

    add default unoptimized realization of selectByIds

--- repos/exposed/src/jvmMain/kotlin/dev/inmo/micro_utils/repos/exposed/CommonExposedRepo.kt
+++ repos/exposed/src/jvmMain/kotlin/dev/inmo/micro_utils/repos/exposed/CommonExposedRepo.kt
@@ -6,4 +6,15 @@ interface CommonExposedRepo<IdType, ObjectType> : ExposedRepo {
     val ResultRow.asObject: ObjectType
     val selectById: SqlExpressionBuilder.(IdType) -> Op<Boolean>
     val selectByIds: SqlExpressionBuilder.(List<IdType>) -> Op<Boolean>
+        get() = { list ->
+            if (list.isEmpty()) {
+                Op.FALSE
+            } else {
+                var op = selectById(list.first())
+                (1 until list.size).forEach {
+                    op = op.and(selectById(list[it]))
+                }
+                op
+            }
+        }
 }
