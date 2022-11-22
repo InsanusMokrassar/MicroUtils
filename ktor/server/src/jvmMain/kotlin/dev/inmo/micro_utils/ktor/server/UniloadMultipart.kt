b7b5159e9c1526e77e6fed021f7e0290d699ecea
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-11-22 09:51:10 +0600

    Revert "Revert "start adding uniupload""
    This reverts commit 0d1aae0ef7bb25ef154f2ce951ceb7fa7a6d3418.

--- ktor/server/src/jvmMain/kotlin/dev/inmo/micro_utils/ktor/server/UniloadMultipart.kt
+++ ktor/server/src/jvmMain/kotlin/dev/inmo/micro_utils/ktor/server/UniloadMultipart.kt
@@ -2,22 +2,10 @@ package dev.inmo.micro_utils.ktor.server
 
 import dev.inmo.micro_utils.common.*
 import dev.inmo.micro_utils.coroutines.safely
-import dev.inmo.micro_utils.ktor.common.*
-import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.call
-import io.ktor.server.request.receive
 import io.ktor.server.request.receiveMultipart
-import io.ktor.server.response.respond
-import io.ktor.server.response.respondBytes
-import io.ktor.server.routing.Route
-import io.ktor.server.websocket.WebSocketServerSession
-import io.ktor.util.pipeline.PipelineContext
 import io.ktor.utils.io.core.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.serialization.DeserializationStrategy
-import kotlinx.serialization.SerializationStrategy
 
 suspend fun ApplicationCall.uniloadMultipart(
     onFormItem: (PartData.FormItem) -> Unit = {},
@@ -88,24 +76,4 @@ suspend fun ApplicationCall.uniloadMultipartFile(
     }
 
     resultInput ?: error("Bytes has not been received")
-}
-
-suspend fun ApplicationCall.getParameterOrSendError(
-    field: String
-) = parameters[field].also {
-    if (it == null) {
-        respond(HttpStatusCode.BadRequest, "Request must contains $field")
-    }
-}
-
-fun ApplicationCall.getQueryParameter(
-    field: String
-) = request.queryParameters[field]
-
-suspend fun ApplicationCall.getQueryParameterOrSendError(
-    field: String
-) = getQueryParameter(field).also {
-    if (it == null) {
-        respond(HttpStatusCode.BadRequest, "Request query parameters must contains $field")
-    }
 }
