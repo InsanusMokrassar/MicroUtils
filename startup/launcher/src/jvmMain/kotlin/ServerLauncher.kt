2a4570eafce07d07e23fdc80ddfd499e8e660745
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-12-05 21:52:36 +0600

    Update ServerLauncher.kt

--- startup/launcher/src/jvmMain/kotlin/ServerLauncher.kt
+++ startup/launcher/src/jvmMain/kotlin/ServerLauncher.kt
@@ -10,15 +10,23 @@ import org.koin.core.module.Module
 import org.koin.dsl.module
 import java.io.File
 
+package dev.inmo.micro_utils.startup.launcher
+
+import dev.inmo.
+
+class ServerLauncher : ServerPlugin {
+    val defaultJson = Json {
+        ignoreUnknownKeys = true
+    }
+
 suspend fun main(args: Array<String>) {
 
     KSLog.default = KSLog("PlaguBot")
     val (configPath) = args
     val file = File(configPath)
     KSLog.i("Start read config from ${file.absolutePath}")
-    val json = defaultJsonFormat.parseToJsonElement(file.readText()).jsonObject
-    val config = defaultJsonFormat.decodeFromJsonElement(Config.serializer(), json)
+    val json = defaultJson.parseToJsonElement(file.readText()).jsonObject
     KSLog.i("Config has been read")
 
-    PlaguBot(json, config).start().join()
+    ServerLauncher.start().join()
 }
