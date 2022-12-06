b9d8528599554f4df5db7114b1a324fae31472d8
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-12-06 13:14:05 +0600

    complete kdocs for the startup module

--- startup/launcher/src/jvmMain/kotlin/Main.kt
+++ startup/launcher/src/jvmMain/kotlin/Main.kt
@@ -5,6 +5,25 @@ import dev.inmo.kslog.common.i
 import kotlinx.serialization.json.jsonObject
 import java.io.File
 
+/**
+ * It is expected, that [args] will contain ONE argument with path to the config json. Sample of launching:
+ *
+ * ```bash
+ * ./gradlew run --args="sample.config.json"
+ * ```
+ *
+ * Content of `sample.config.json` described in [Config] KDocs.
+ *
+ * You may build runnable app using:
+ *
+ * ```bash
+ * ./gradlew assembleDist
+ * ```
+ *
+ * In that case in `build/distributions` folder you will be able to find zip and tar files with all required
+ * tools for application running (via their `bin/app_name` binary). In that case yoy will not need to pass
+ * `--args=...` and launch will look like `./bin/app_name sample.config.json`
+ */
 suspend fun main(args: Array<String>) {
 
     KSLog.default = KSLog("ServerLauncher")
