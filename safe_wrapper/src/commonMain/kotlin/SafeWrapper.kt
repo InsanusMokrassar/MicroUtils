ced05a458606e1359e5aaeb096cd6fa2ae3e5473
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-12-09 12:14:24 +0600

    improve default runCatchingSafely/safelyWithResult and add suspend variances of safe/unsafe SafeWrapper interface

--- safe_wrapper/src/commonMain/kotlin/SafeWrapper.kt
+++ safe_wrapper/src/commonMain/kotlin/SafeWrapper.kt
@@ -1,8 +1,12 @@
 package dev.inmo.micro_utils.safe_wrapper
 
+import dev.inmo.micro_utils.coroutines.runCatchingSafely
+
 interface SafeWrapper<T> {
-    fun <R> safe (block: T.() -> R): Result<R> = unsafeTarget().runCatching(block)
+    fun <R> safe(block: T.() -> R): Result<R> = unsafeTarget().runCatching(block)
     fun <R> unsafe(block: T.() -> R): R = unsafeTarget().block()
+    suspend fun <R> safeS(block: suspend T.() -> R): Result<R> = unsafeTarget().runCatchingSafely(block = block)
+    suspend fun <R> unsafeS(block: suspend T.() -> R): R = unsafeTarget().block()
     fun unsafeTarget(): T
 
     class Default<T>(private val t: T) : SafeWrapper<T> { override fun unsafeTarget(): T = t }
