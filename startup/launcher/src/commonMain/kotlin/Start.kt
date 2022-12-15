c584c24fcee596df509368ef0718f139cea94429
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-12-15 15:07:10 +0600

    fixes and improvements

--- startup/launcher/src/commonMain/kotlin/Start.kt
+++ startup/launcher/src/commonMain/kotlin/Start.kt
@@ -3,7 +3,7 @@ package dev.inmo.micro_utils.startup.launcher
 import dev.inmo.kslog.common.i
 import kotlinx.serialization.json.JsonObject
 import org.koin.core.KoinApplication
-import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 /**
@@ -23,7 +23,7 @@ suspend fun start(rawConfig: JsonObject) {
             }
         )
         logger.i("Modules loaded")
-        GlobalContext.startKoin(koinApp)
+        startKoin(koinApp)
         logger.i("Koin started")
         startPlugin(koinApp.koin)
         logger.i("App has been setup")
