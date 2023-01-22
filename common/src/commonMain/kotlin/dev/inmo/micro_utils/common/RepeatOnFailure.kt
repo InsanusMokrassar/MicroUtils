20799b9a3e2e28f49003c27476dca24a53e878d6
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-01-22 22:43:26 +0600

    add repeat on failure with callback

--- common/src/commonMain/kotlin/dev/inmo/micro_utils/common/RepeatOnFailure.kt
+++ common/src/commonMain/kotlin/dev/inmo/micro_utils/common/RepeatOnFailure.kt
@@ -6,16 +6,49 @@ package dev.inmo.micro_utils.common
  * A zero-based index of current iteration is passed as a parameter to [action].
  */
 inline fun <R> repeatOnFailure(
+    onFailure: (Throwable) -> Boolean,
+    action: () -> R
+): Result<R> {
+    do {
+        runCatching {
+            action()
+        }.onFailure {
+            if (!onFailure(it)) {
+                return Result.failure(it)
+            }
+        }.onSuccess {
+            return Result.success(it)
+        }
+    } while (true)
+}
+
+/**
+ * Executes the given [action] until getting of successful result specified number of [times].
+ *
+ * A zero-based index of current iteration is passed as a parameter to [action].
+ */
+inline fun <R> repeatOnFailure(
     times: Int,
     onEachFailure: (Throwable) -> Unit = {},
     action: (Int) -> R
 ): Optional<R> {
-    repeat(times) {
-        runCatching {
-            action(it)
-        }.onFailure(onEachFailure).onSuccess {
-            return Optional.presented(it)
+    var i = 0
+    val result = repeatOnFailure(
+        {
+            onEachFailure(it)
+            if (i < times) {
+                i++
+                true
+            } else {
+                false
+            }
         }
+    ) {
+        action(i)
     }
-    return Optional.absent()
+    return if (result.isSuccess) {
+        Optional.presented(result.getOrThrow())
+    } else {
+        Optional.absent()
+    }
 }
