2645ea29d6a3c494298b28874d42db8ee5cce718
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-01-31 00:11:45 +0600

    Update ActionWrapper.kt

--- repos/cache/src/commonMain/kotlin/dev/inmo/micro_utils/repos/cache/fallback/ActionWrapper.kt
+++ repos/cache/src/commonMain/kotlin/dev/inmo/micro_utils/repos/cache/fallback/ActionWrapper.kt
@@ -3,9 +3,23 @@ package dev.inmo.micro_utils.repos.cache.fallback
 import dev.inmo.micro_utils.coroutines.runCatchingSafely
 import kotlinx.coroutines.withTimeout
 
+/**
+ * Realizations should [wrap] the work with some conditions like retries on exceptions, calling timeout, etc.
+ * 
+ * @see Timeouted
+ * @see Direct
+ */
 interface ActionWrapper {
+    /**
+     * Should execute [block] to take the result [T], but may return failure in case when something went wrong.
+     * This method should never throw any [Exception]
+     */
     suspend fun <T> wrap(block: suspend () -> T): Result<T>
 
+    /**
+     * This type of [ActionWrapper]s will use [withTimeout]([timeoutMillis]) and if original call
+     * will not return anything in that timeout just return [Result] with failure
+     */
     class Timeouted(private val timeoutMillis: Long) : ActionWrapper {
         override suspend fun <T> wrap(block: suspend () -> T): Result<T> = runCatchingSafely {
             withTimeout(timeoutMillis) {
@@ -13,7 +27,11 @@ interface ActionWrapper {
             }
         }
     }
+    /**
+     * It is passthrough variant of [ActionWrapper] which will just call incoming block with wrapping into [runCatchingSafely]
+     */
     object Direct : ActionWrapper {
+
         override suspend fun <T> wrap(block: suspend () -> T): Result<T> = runCatchingSafely { block() }
     }
 }
