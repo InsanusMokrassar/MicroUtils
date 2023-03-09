b3f468f9018a40c2d8dda9ad425aa91cc61c723c
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-03-09 22:21:11 +0600

    add docs to mapper serialization and allow to use them as supertypes

--- serialization/mapper/src/commonMain/kotlin/MapperDeserializationStrategy.kt
+++ serialization/mapper/src/commonMain/kotlin/MapperDeserializationStrategy.kt
@@ -1,30 +1,26 @@
 package dev.inmo.micro_utils.serialization.mapper
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 /**
- * Use this serializer when you have serializable type [I] and want to map it to some [O] in process of
- * serialization/deserialization
+ * Use this serializer when you have deserializable type [I] and want to map it to some [O] in process of
+ * deserialization
  *
  * @param base Serializer for [I]
- * @param serialize Will be used in [serialize] method to convert incoming [O] to [I] and serialize with [base]
  * @param deserialize Will be used in [deserialize] method to convert deserialized by [base] [I] to [O]
  */
-open class MapperSerializer<I, O>(
-    private val base: KSerializer<I>,
-    private val serialize: (O) -> I,
+open class MapperDeserializationStrategy<I, O>(
+    private val base: DeserializationStrategy<I>,
     private val deserialize: (I) -> O
-) : KSerializer<O> {
+) : DeserializationStrategy<O> {
     override val descriptor: SerialDescriptor = base.descriptor
 
     override fun deserialize(decoder: Decoder): O {
         return deserialize(base.deserialize(decoder))
-    }
-
-    override fun serialize(encoder: Encoder, value: O) {
-        base.serialize(encoder, serialize(value))
     }
 }
--- serialization/mapper/src/commonMain/kotlin/MapperSerializationStrategy.kt
+++ serialization/mapper/src/commonMain/kotlin/MapperSerializationStrategy.kt
@@ -1,28 +1,23 @@
 package dev.inmo.micro_utils.serialization.mapper
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 /**
  * Use this serializer when you have serializable type [I] and want to map it to some [O] in process of
- * serialization/deserialization
+ * serialization
  *
  * @param base Serializer for [I]
  * @param serialize Will be used in [serialize] method to convert incoming [O] to [I] and serialize with [base]
- * @param deserialize Will be used in [deserialize] method to convert deserialized by [base] [I] to [O]
  */
-open class MapperSerializer<I, O>(
-    private val base: KSerializer<I>,
-    private val serialize: (O) -> I,
-    private val deserialize: (I) -> O
-) : KSerializer<O> {
+open class MapperSerializationStrategy<I, O>(
+    private val base: SerializationStrategy<I>,
+    private val serialize: (O) -> I
+) : SerializationStrategy<O> {
     override val descriptor: SerialDescriptor = base.descriptor
-
-    override fun deserialize(decoder: Decoder): O {
-        return deserialize(base.deserialize(decoder))
-    }
 
     override fun serialize(encoder: Encoder, value: O) {
         base.serialize(encoder, serialize(value))
