afc6aeea159d88a076e1f975ef63c8c5c99c1b16
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-03-10 16:14:51 +0600

    fixes

--- repos/common/src/commonMain/kotlin/dev/inmo/micro_utils/repos/pagination/CRUDPaginationExtensions.kt
+++ repos/common/src/commonMain/kotlin/dev/inmo/micro_utils/repos/pagination/CRUDPaginationExtensions.kt
@@ -1,12 +1,19 @@
 package dev.inmo.micro_utils.repos.pagination
 
+import dev.inmo.micro_utils.common.toCoercedInt
 import dev.inmo.micro_utils.pagination.*
 import dev.inmo.micro_utils.pagination.utils.getAllWithNextPaging
 import dev.inmo.micro_utils.repos.ReadCRUDRepo
 
 suspend inline fun <T, ID, REPO : ReadCRUDRepo<T, ID>> REPO.getAll(
+    pagination: Pagination,
     @Suppress("REDUNDANT_INLINE_SUSPEND_FUNCTION_TYPE")
     crossinline methodCaller: suspend REPO.(Pagination) -> PaginationResult<T>
-): List<T> = getAllWithNextPaging {
+): List<T> = getAllWithNextPaging(pagination) {
     methodCaller(this, it)
 }
+
+suspend inline fun <T, ID, REPO : ReadCRUDRepo<T, ID>> REPO.getAll(
+    @Suppress("REDUNDANT_INLINE_SUSPEND_FUNCTION_TYPE")
+    crossinline methodCaller: suspend REPO.(Pagination) -> PaginationResult<T>
+): List<T> = getAll(FirstPagePagination(count().toCoercedInt()), methodCaller)
