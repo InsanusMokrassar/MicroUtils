4901a8844cf2ca3a25b6bedf46eecfed367f0ecf
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-05-12 21:34:17 +0600

    complete rework of states

--- coroutines/src/commonTest/kotlin/SpecialMutableStateFlowTests.kt
+++ coroutines/src/commonTest/kotlin/SpecialMutableStateFlowTests.kt
@@ -1,35 +1,33 @@
 import dev.inmo.micro_utils.coroutines.SpecialMutableStateFlow
+import dev.inmo.micro_utils.coroutines.asDeferred
 import dev.inmo.micro_utils.coroutines.subscribe
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class SpecialMutableStateFlowTests {
     @Test
-    fun simpleTest() {
+    fun simpleTest() = runTest {
         val specialMutableStateFlow = SpecialMutableStateFlow(0)
-        runTest {
-            specialMutableStateFlow.value = 1
-            specialMutableStateFlow.first { it == 1 }
-        }
+        specialMutableStateFlow.value = 1
+        specialMutableStateFlow.first { it == 1 }
         assertEquals(1, specialMutableStateFlow.value)
     }
     @Test
-    fun specialTest() {
+    fun specialTest() = runTest {
         val specialMutableStateFlow = SpecialMutableStateFlow(0)
-        runTest {
-            lateinit var subscriberJob: Job
-            subscriberJob = specialMutableStateFlow.subscribe(this) {
-                when (it) {
-                    1 -> specialMutableStateFlow.value = 2
-                    2 -> subscriberJob.cancel()
-                }
+        lateinit var subscriberJob: Job
+        subscriberJob = specialMutableStateFlow.subscribe(this) {
+            when (it) {
+                1 -> specialMutableStateFlow.value = 2
+                2 -> subscriberJob.cancel()
             }
-            specialMutableStateFlow.value = 1
-            subscriberJob.join()
         }
+        specialMutableStateFlow.value = 1
+        subscriberJob.join()
         assertEquals(2, specialMutableStateFlow.value)
     }
 }
