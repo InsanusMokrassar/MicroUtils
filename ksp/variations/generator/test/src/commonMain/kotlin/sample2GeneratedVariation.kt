99783f281dada1bacadbed7e68550973b169e26e
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2025-02-12 12:37:08 +0600

    generate sample functions

---
+++ ksp/variations/generator/test/src/commonMain/kotlin/sample2GeneratedVariation.kt
@@ -0,0 +1,47 @@
+// THIS CODE HAVE BEEN GENERATED AUTOMATICALLY
+// TO REGENERATE IT JUST DELETE FILE
+// ORIGINAL FILE: SampleFun.kt
+import kotlin.Boolean
+import kotlin.Int
+import kotlin.String
+import kotlin.Unit
+
+public suspend fun Sample.sample2(arg12: Int): Unit = sample2(
+    arg1 = arg12.toString()
+)
+
+public suspend fun Sample.sample2(arg12: Int, arg2: Int): Unit = sample2(
+    arg1 = arg12.toString(), arg2 = arg2
+)
+
+public suspend fun Sample.sample2(arg12: Int, arg3: Boolean): Unit = sample2(
+    arg1 = arg12.toString(), arg3 = arg3
+)
+
+public suspend fun Sample.sample2(
+  arg12: Int,
+  arg2: Int,
+  arg3: Boolean,
+): Unit = sample2(
+    arg1 = arg12.toString(), arg2 = arg2, arg3 = arg3
+)
+
+public suspend fun Sample.sample2(arg22: String): Unit = sample2(
+    arg2 = arg22.toInt()
+)
+
+public suspend fun Sample.sample2(arg1: String, arg22: String): Unit = sample2(
+    arg1 = arg1, arg2 = arg22.toInt()
+)
+
+public suspend fun Sample.sample2(arg22: String, arg3: Boolean): Unit = sample2(
+    arg2 = arg22.toInt(), arg3 = arg3
+)
+
+public suspend fun Sample.sample2(
+  arg1: String,
+  arg22: String,
+  arg3: Boolean,
+): Unit = sample2(
+    arg1 = arg1, arg2 = arg22.toInt(), arg3 = arg3
+)
---
+++ ksp/variations/generator/test/src/commonMain/kotlin/sampleGeneratedVariation.kt
@@ -0,0 +1,8 @@
+// THIS CODE HAVE BEEN GENERATED AUTOMATICALLY
+// TO REGENERATE IT JUST DELETE FILE
+// ORIGINAL FILE: SampleFun.kt
+import kotlin.Unit
+
+public fun sample(example: Sample): Unit = sample(
+    example = example.value
+)
