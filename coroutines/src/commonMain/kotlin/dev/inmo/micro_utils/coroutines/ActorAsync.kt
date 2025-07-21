3bbf978b0048eaa7860d15d91704cb509fe35458
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2025-07-21 22:02:30 +0600

    fixes in actorAsync

--- coroutines/src/commonMain/kotlin/dev/inmo/micro_utils/coroutines/ActorAsync.kt
+++ coroutines/src/commonMain/kotlin/dev/inmo/micro_utils/coroutines/ActorAsync.kt
@@ -1,5 +1,6 @@
 package dev.inmo.micro_utils.coroutines
 
+import dev.inmo.kslog.common.KSLog
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.consumeAsFlow
@@ -7,13 +8,15 @@ import kotlinx.coroutines.flow.consumeAsFlow
 fun <T> CoroutineScope.actorAsync(
     channelCapacity: Int = Channel.UNLIMITED,
     markerFactory: suspend (T) -> Any? = { null },
+    logger: KSLog = KSLog,
     block: suspend (T) -> Unit
 ): Channel<T> {
     val channel = Channel<T>(channelCapacity)
-    channel.consumeAsFlow().subscribeAsync(this, markerFactory, block)
+    channel.consumeAsFlow().subscribeAsync(this, markerFactory, logger, block)
     return channel
 }
 
+@Deprecated("Use standard actosAsync instead", ReplaceWith("actorAsync(channelCapacity, markerFactory, block = block)", "dev.inmo.micro_utils.coroutines.actorAsync"))
 inline fun <T> CoroutineScope.safeActorAsync(
     channelCapacity: Int = Channel.UNLIMITED,
     noinline onException: ExceptionHandler<Unit> = defaultSafelyExceptionHandler,
